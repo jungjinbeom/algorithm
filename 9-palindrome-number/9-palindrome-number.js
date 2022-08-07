5c4dbbc9e88c17c632c4ca60f36ea3bf923e3b99
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2022-08-07 10:38:42 +0900

    Time: 194 ms (87.83%), Space: 50.6 MB (82.88%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,15 +3,24 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+
     let answer = true
     let str = String(x);
     let arr = [...str];
-    let len = arr.length;
-    for(let i =0; Math.floor(len/2)>i; i++){
-         if(arr[i]!==arr[len-1-i]){
-         answer=false
-     }
+    
+    //첫번째 예시
+    // let len = arr.length;
+    // for(let i =0; Math.floor(len/2)>i; i++){
+    //      if(arr[i]!==arr[len-1-i]){
+    //      answer=false
+    //  }
+    // }
+    
+    // 두번째 예시 
+    let arrNum = arr.reverse('').join('');
+    let num = Number(arrNum);
+    if(num!==x){
+        answer =false
     }
- 
     return answer
 };
