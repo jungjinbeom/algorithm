54b3797a5960d6d815903564e00da44824adf817
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2022-08-07 10:27:23 +0900

    Time: 171 ms (96.01%), Space: 51.4 MB (29.42%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -8,8 +8,7 @@ var isPalindrome = function(x) {
     let arr = [...str];
     let len = arr.length;
     for(let i =0; Math.floor(len/2)>i; i++){
-       console.log(arr[len-1-i])
-     if(arr[i]!==arr[len-1-i]){
+         if(arr[i]!==arr[len-1-i]){
          answer=false
      }
     }
