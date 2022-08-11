1bddcd09a39f58be1011e5ef2b6ec5da1ec2e292
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2022-08-11 20:47:50 +0900

    Time: 230 ms (32.44%), Space: 47.8 MB (39.00%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -14,15 +14,11 @@ var romanToInt = function(s) {
     let answer = 0;
     for(let i =0;s.length>i;i++){
         if(code[s[i]]<code[s[i+1]]){
-  
             answer = answer + code[s[i+1]]-code[s[i]]
             i++
            }else{
             answer = answer+ code[s[i]]
         }
-        console.log("a")
     }
-   
-   
     return answer
 };
