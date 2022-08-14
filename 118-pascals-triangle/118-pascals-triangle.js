9dde0d049f3527899e8c9b220d7eefa6b24d1e63
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2022-08-14 12:15:04 +0900

    Time: 86 ms (50.61%), Space: 41.6 MB (90.39%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -5,16 +5,17 @@
 var generate = function(numRows) {
     var answer=[]
         for(let i = 0;numRows>i;i++){
-            if(!i || i===1) {
-                !i ? answer.push([1]) : answer.push([1,1])
+            if(i === 0 || i === 1) {
+                i ? answer.push([1,1]) : answer.push([1])
             }else{   
-                let prevArr = answer[i-1]
-                const result =prevArr.reduce((acc,curr,idx)=>{
-                    let nextNum = prevArr[idx+1];
+                
+                const result =answer[i-1].reduce((acc,curr,idx)=>{                 
+                    let nextNum = answer[i-1][idx+1];
                     if(i-1===idx){
                         acc.push(curr);
                     }else{
-                        idx ? acc.push(nextNum+curr):acc.push(curr,nextNum+curr)
+                        let plusNum = answer[i-1][idx+1]+curr
+                        idx ? acc.push(plusNum):acc.push(curr,plusNum)
                     }
                    return acc
                },[]);
