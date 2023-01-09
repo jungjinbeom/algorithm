3cdd8d4593dedecefdcdc230df0f6c27bdaabe3c
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2023-01-09 22:35:05 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,15 +1,7 @@
 function solution(array, commands) {
-    var answer = [];
-    for(let idx = 0;commands.length>idx;idx++){
-        const arr = [...array]
-        const [i,j,k] =commands[idx];
-        const result = arr.slice(i-1,j).sort((a,b)=>a-b)
-        answer.push(result.length > 2 ? result[k-1] : result[commands.length%k])
-        
-        
-    }
-    
-    
-    
-    return answer;
+    return commands.map((command,idx)=>{
+        const [i,j,k] = command;
+        const result = array.slice(i-1,j).sort((a,b)=>a-b);
+        return result[k-1];
+    });
 }
