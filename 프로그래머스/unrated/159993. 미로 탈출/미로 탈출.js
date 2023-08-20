9e60d3fd8cd1a4b29ba6ce618917dd221fe5b7c4
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2023-08-20 21:42:40 +0900

    [unrated] Title: 미로 탈출, Time: 0.48 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.js
@@ -0,0 +1,57 @@
+const DIRECTIONS = [
+[0, -1],
+[0, 1],
+[-1, 0],
+[1, 0],
+];
+
+function solution(maps) {
+const mapYLen = maps.length;
+const mapXLen = maps[0].length;
+
+const getMaps = () => maps.map(line => line.split(''));
+
+const isValidPosition = (y, x) => y >= 0 && y < mapYLen && x >= 0 && x < mapXLen;
+
+const bfs = (position, target) => {
+const visited = Array.from({ length: mapYLen }, () => Array(mapXLen).fill(false));
+const queue = [[...position, 0]];
+
+while (queue.length > 0) {
+const [y, x, count] = queue.shift();
+
+if (!isValidPosition(y, x) || visited[y][x] || maps[y][x] === 'X') continue;
+if (maps[y][x] === target) return count;
+
+visited[y][x] = true;
+
+DIRECTIONS.forEach(([dY, dX]) => {
+const nY = y + dY;
+const nX = x + dX;
+queue.push([nY, nX, count + 1]);
+});
+}
+
+return -1;
+};
+
+let start;
+let lever;
+
+for (let i = 0; i < mapYLen; i++) {
+for (let j = 0; j < mapXLen; j++) {
+if (maps[i][j] === 'S') start = [i, j];
+if (maps[i][j] === 'L') lever = [i, j];
+}
+}
+
+const leverToExit = bfs(lever, 'E');
+if (leverToExit === -1) return -1;
+
+maps = getMaps(); // Reset the maps array for the second BFS
+
+const startToLever = bfs(start, 'L');
+if (startToLever === -1) return -1;
+
+return startToLever + leverToExit;
+}
