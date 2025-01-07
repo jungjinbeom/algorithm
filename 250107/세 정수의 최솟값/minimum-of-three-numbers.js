15c17c4ebf03017ba119ae70c71d40cddf498308
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-07 22:53:34 +0900

    Title: 세 정수의 최솟값, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250107/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 250107/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -3,16 +3,17 @@ const fs = require('fs');
 let input = fs.readFileSync(0).toString().split(' ').map(Number);
 const [a,b,c] = input;
 
-let res = 0
-if(a<b && a<c){
+let res = 0;
+
+if(a<=b && a<=c){
     res = a
 }
 
-if(b<a && b<c){
+if(b<=a && b<=c){
     res = b
 }
 
-if(c<a && c<b){
+if(c<=a && c<=b){
     res = c
 }
 console.log(res)
