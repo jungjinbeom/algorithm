de5be72500ef071f5acdd5d01f151eb4256f5787
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-07 21:38:08 +0900

    Title: 시력 검사 2, Time: 163ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250107/시력 검사 2/eye-test-2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+let input = Number(fs.readFileSync(0).toString())
+
+if(input >= 1){
+    console.log('High')
+}else if(input >= 0.5 ){
+    console.log('Middle')
+}else{
+    console.log('Low')
+}
