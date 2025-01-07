03501f4c78d54f5969387eed51a1fb5f30ac07a1
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-07 23:49:00 +0900

    Title: 최댓값 구하기, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 250107/최댓값 구하기/maximum-value.js
+++ 250107/최댓값 구하기/maximum-value.js
@@ -4,14 +4,17 @@ let input = fs.readFileSync(0).toString().split(' ').map(Number);
 
 const [a,b,c]=input;
 
-if(a>b && a>c){
-    console.log(a)
-}
-
-if(b>a && b>c){
-    console.log(b)
-}
-
-if(c>a && c>b){
-    console.log(c)
+if(a>=b){
+    if(a>=c){
+        console.log(a)
+    }else{
+        console.log(c)
+    }
+    
+}else{
+    if(b>=c){
+        console.log(b)
+    }else{
+        console.log(c)
+    }
 }
