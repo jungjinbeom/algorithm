e07b6c5058587b1e08d358850eb6da42d92f742d
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-07 21:49:43 +0900

    Title: 살 수 있는 물건 2, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250107/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let input = Number(fs.readFileSync(0).toString())
+
+if(input>=3000){
+    console.log('book')
+}else if(input>=1000){
+    console.log('mask')
+}else if(input>=500){
+    console.log('pen')
+}else{
+    console.log('no')
+}
