d5b6d327d6f6c0c91a2f7bf274b7ea6bc9ab9fc2
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-08 12:32:57 +0900

    Title: 정수 입력받아 배수 출력, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 250108/정수 입력받아 배수 출력/print-multiple-of-input.js
+++ 250108/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 let input = Number(fs.readFileSync(0).toString());
 
-let result = ''
-for(let i = 1; input * 5>=i; i++){
-    if(i%input===0){
-        result += (i+(" "));
-    }
+let result = '';
+
+for(let i = 1; 5>=i; i++){
+        result += ((input * i) +(" "));
 }
+
 console.log(result)
 
