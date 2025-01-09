9735cd173c92c9166b76411f215a179dc1aa22f8
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-09 11:52:51 +0900

    Title: 짝수만 출력, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250109/짝수만 출력/output-only-even.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let [a,b] = fs.readFileSync(0).toString().split(' ').map(Number);
+
+let result = ''
+for(let i = a; b>=i; i++){
+    if(i%2===0){
+        result += (i+(" "));
+    }
+}
+console.log(result)
+
+
