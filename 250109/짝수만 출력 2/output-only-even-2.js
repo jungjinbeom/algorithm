e6d6c414d837613e41576c068e40567b53a9e8e6
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-09 12:00:08 +0900

    Title: 짝수만 출력 2, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 250109/짝수만 출력 2/output-only-even-2.js
+++ 250109/짝수만 출력 2/output-only-even-2.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
-let [b,a] = fs.readFileSync(0).toString().split(' ');
+let [b,a] = fs.readFileSync(0).toString().split(' ').map(Number);
 
-let result = ''
+let result = '';
+
 for(let i = b; i>=a; i--){
-    if(i%2===0){
-        result += (i+(' '));
+    if(i % 2 === 0){
+        result += i+(' ');
     }
 }
 
