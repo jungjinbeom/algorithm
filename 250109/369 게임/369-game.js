aaeab073cb17f94f08815b93dfd72f4179b7de72
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-09 13:00:39 +0900

    Title: 369 게임, Time: 173ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250109/369 게임/369-game.js
+++ 250109/369 게임/369-game.js
@@ -4,8 +4,9 @@ let n = Number(fs.readFileSync(0).toString().trim())
 
 let r = ''
 
+console.log(31 % 10 )
 for(let i = 1; n >= i; i++){
-    if(i % 3 === 0 || i % 10 ===3 || i % 10 === 6 || i % 10 === 9){
+    if(i % 3 === 0 || i % 10 ===3 || i % 10 === 6 || i % 10 === 9 || i / 10 === 3 || i / 10 === 6 || i / 10 === 9){
         r += 0 + (' ');
     }else{
         r += i + (' ');
