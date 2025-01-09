f63e44fd7d7e56e4e8cb76ea5e67f822c661ba8b
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-09 12:38:11 +0900

    Title: 높은 수에서 낮은 수까지, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 250109/높은 수에서 낮은 수까지/from-high-to-low.js
+++ 250109/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 
 let [a,b] = fs.readFileSync(0).toString().split(' ').map(Number);
 
+let n = a>b ? a : b ;
+let n2 = a>b ? b : a ;
+let result = '';
 
-let result = ''
-for(let i = b; i>=a; i --){
+for(let i = n; i>=n2; i --){
     result += i + (' ')
 }
 console.log(result)
