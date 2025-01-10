8b99d5b9dbfb944c1d58d8e65b66a90edc50b8a6
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-11 00:10:37 +0900

    Title: 총합 구하기, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250111/총합 구하기/find-the-total.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+let [a,b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let sumVal = 0
+for(let i = a; b>=i; i++){
+    if(i%6===0 && i%8 !==0){
+        sumVal += i
+    }
+}
+
+console.log(sumVal)
