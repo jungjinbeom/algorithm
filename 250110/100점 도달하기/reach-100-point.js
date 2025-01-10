79eeb3231d22d38cd8d69aa0959af26b96cedc37
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-10 12:11:38 +0900

    Title: 100점 도달하기, Time: 194ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250110/100점 도달하기/reach-100-point.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let result = ''
+for(let i = n; 100>=i; i++){
+    if(i>=90){
+        result += "A" + (' ')
+        continue;
+    }
+    if(i>=80){
+        result += "B" + (' ')
+        continue;
+    }
+    if(i>=70){
+        result += "C" + (' ')
+        continue;
+    }
+    if(i>=60){
+        result += "D" + (' ')
+        continue;
+    }
+    if(i<60){
+        result += "E" + (' ')
+        continue;
+    }    
+}
+
+   console.log(result)
