d7969a2d54827262cf6a40dfda1adc42f92384a2
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-10 23:17:01 +0900

    Title: 배수의 개수, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 250110/배수의 개수/number-of-multipliers.js
+++ 250110/배수의 개수/number-of-multipliers.js
@@ -1,14 +1,17 @@
 const fs = require('fs')
 
-let n = fs.readFileSync(0).toString().split('\n').map(Number);
+let n = fs.readFileSync(0).toString().split('\n').filter(item=>item).map(Number)
 
 let r = 0;
 let r2 =0;
+
 for(let i = 0; n.length>i;i++){
+   
     if(n[i]%3===0){
         r++
     }
     if(n[i]%5===0){
+        //console.log(n[i])
         r2++
     }
 }
