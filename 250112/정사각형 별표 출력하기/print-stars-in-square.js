c6969fbc8f7e2c67aad6d6d645ea1894b14726d4
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-12 22:32:11 +0900

    Title: 정사각형 별표 출력하기, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250112/정사각형 별표 출력하기/print-stars-in-square.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ''
+for(let i = 0; n>i; i++){
+    str =''
+    for(let j = 0; n>j; j++){
+        str +='*'
+    }
+    console.log(str)
+}
