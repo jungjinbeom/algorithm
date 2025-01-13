88cbdae7e408873c96c7b0e5013a425c6cfd117b
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-13 12:42:03 +0900

    Title: 별표 출력하기 3, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250113/별표 출력하기 3/print-star-3.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = '';
+
+for(let i = 0; n>i; i++){
+    str = ''
+    for(let j = 0; i > j; j++){
+        str +="  "
+    }
+
+    for(let k = 0; (2 * n) - (2 * i) - 1 > k; k++){
+        str +='* '
+    }
+    console.log(str)
+}
