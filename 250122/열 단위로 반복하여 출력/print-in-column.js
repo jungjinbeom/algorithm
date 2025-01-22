782ac196048bd61d5f127b38d2446f4b1f0836be
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-22 21:17:50 +0900

    Title: 열 단위로 반복하여 출력, Time: 160ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250122/열 단위로 반복하여 출력/print-in-column.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+let input = Number(fs.readFileSync(0).toString().trim());
+
+let str = ''
+
+for(let i = 1; input>=i;i++){
+    str=''
+    for(let j = 1; input>=j;j++){
+        str+=String(i)
+    }
+    console.log(str)
+}
