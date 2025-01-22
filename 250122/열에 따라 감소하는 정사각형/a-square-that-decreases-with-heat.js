b3580354fcf7fa4cf21f98e969cb12999cc5a94c
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-01-22 21:25:35 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250122/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.js
@@ -0,0 +1,12 @@
+const fs =require('fs')
+
+let input = Number(fs.readFileSync(0).toString().trim())
+
+let str = ''
+for(let i = input; i>0;i--){
+    str=''
+    for(let j = input; j>0; j--){
+        str+= `${String(j)} `
+    }
+    console.log(str)
+}
