3cb1f4fe4f00a9d0963fd97bdd61a386ce392264
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-02-16 23:25:15 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

--- 250216/시험 통과 여부 확인하기 2/verify-test-passed-2.js
+++ 250216/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -5,10 +5,9 @@ let input = fs.readFileSync(0).toString().trim().split('\n')
 const n = input[0]
 
 let cnt = 0
-let r = 0
 
 for(let i = 1; n>=i;i++ ){
-    r = 0
+    let r = 0
     const score = input[i].split(' ')
     for(let j = 0; score.length-1>=j;j++){
         r += Number(score[j]);
