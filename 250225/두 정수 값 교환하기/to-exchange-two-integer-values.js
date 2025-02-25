4a207838ac2f10d89ecf8c2426a7edcfdf67475f
jungjinbeom <62785643+jungjinbeom@users.noreply.github.com>
2025-02-25 20:04:55 +0900

    Title: 두 정수 값 교환하기, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 250225/두 정수 값 교환하기/to-exchange-two-integer-values.js
+++ 250225/두 정수 값 교환하기/to-exchange-two-integer-values.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 
 let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
-const [a,b] = input
+let [a,b] = input
 
-console.log(b, a)
+function swap(a, b) {
+    [a, b] = [b, a];
+    return [a, b];
+}
+
+[a, b] = swap(a,b)
+
+console.log(a, b)
